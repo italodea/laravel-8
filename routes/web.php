6d853ef350d2b7db3e3c135c3a5a296e4ea318b1
italo de azevedo <italodeazevedo99@gmail.com>
2021-06-13 10:39:59 -0300

    Feita a autenticação e registro de usuario

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\{
-    PostController
-};
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,14 +13,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
 Route::get('/linkstorage', function () {
     Artisan::call('storage:link');
 });
-
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::post('/posts',[PostController::class, 'store'])->name('posts.store');
@@ -31,4 +35,3 @@ Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edi
 Route::delete('/posts/{id}',[PostController::class, 'delete'])->name('posts.delete');
 Route::put('/posts/{id}',[PostController::class, 'update'])->name('posts.update');
 Route::any('/posts/search',[PostController::class, 'search'])->name('posts.search');
-
